979fdff46141574db10d3f98e9b6c93be6603a07
ArchUsr64 <anshulusr@gmail.com>
2023-08-05 16:41:19 +0530

    added lfsr rng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+fn lfsr_fib() -> i32 {
+    const START_STATE: i32 = 0xAFD2;
+    static mut LFSR: i32 = START_STATE;
+    let bit;
+
+    unsafe {
+        bit = ((LFSR >> 0) ^ (LFSR >> 2) ^ (LFSR >> 3) ^ (LFSR >> 5)) & 1;
+        LFSR = (LFSR >> 1) | (bit << 15);
+        return LFSR;
+    }
+}
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("{}", lfsr_fib());
+    }
 }
