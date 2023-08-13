6622a864aef7fed370d8c502b6e090c3e1a52697
ArchUsr64 <anshulusr@gmail.com>
2023-08-13 14:46:54 +0530

    added randomized colors for plots

--- generate_graph.py
+++ generate_graph.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as pyplot
+import random
 import numpy
 
 FILE_NAME = "data.csv"
@@ -20,13 +21,22 @@ for line in file_stream:
         first_line = False
     else:
         scores.append(temp_score)
+algorithms = algorithms[1:]
 
 pyplot.style.use('_mpl-gallery')
 
 x = [sample_count[0] for sample_count in scores]
 ys = [[performance_score[i + 1] for performance_score in scores] for i in range(tested_algorithms_count)]
 
+colors = ["r", "g", "b", "yellow", "orange", "brown", "black", "gray"]
+def random_color():
+    index = random.randint(0, len(colors) - 1)
+    color = colors[index]
+    colors.remove(color)
+    return color
+
 fig, ax = pyplot.subplots()
-[ax.plot(x, y) for y in  ys]
+[ax.plot(x, y, f'{random_color()}') for y in  ys]
+pyplot.legend(algorithms)
 
 pyplot.savefig("plot.png", dpi=500)
